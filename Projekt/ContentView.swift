5da291470d711f4f0124b952eaad99cd5a41c10e
Abdulrahman <a14azrak@gmail.com>
2024-05-02 08:30:48 +0200

    Initial Commit

---
+++ Projekt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Projekt
+//
+//  Created by Abdulrahman.Alazrak on 2024-05-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Projekt/ProjektApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjektApp.swift
+//  Projekt
+//
+//  Created by Abdulrahman.Alazrak on 2024-05-02.
+//
+
+import SwiftUI
+
+@main
+struct ProjektApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
