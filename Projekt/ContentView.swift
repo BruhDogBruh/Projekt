47608aca5656d5d07b2a43a34704284b14a6db49
Abdulrahman <a14azrak@gmail.com>
2024-05-02 08:37:27 +0200

    First commit

--- Projekt/ContentView.swift
+++ Projekt/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            //ABC
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
